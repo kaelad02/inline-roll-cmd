4a439a576898c3fc3fd0ba66beb3307003344cc1
Chris Seieroe <kaelad02@users.noreply.github.com>
2024-02-28 14:13:55 -0800

    add migration to system's enrichers

--- src/inline-roll-cmd.js
+++ src/inline-roll-cmd.js
@@ -5,40 +5,53 @@ import { debug, MODULE_NAME } from "./util.js";
  */
 Hooks.once("devModeReady", ({ registerPackageDebugFlag }) => registerPackageDebugFlag(MODULE_NAME));
 
+const skillPattern =
+  /\[\[\/(r|roll|pr|publicroll|gmr|gmroll|br|broll|blindroll|sr|selfroll)Skill (\w+)\]\](?:{([^}]+)})?/gi;
+const abilityPattern =
+  /\[\[\/(r|roll|pr|publicroll|gmr|gmroll|br|broll|blindroll|sr|selfroll)Ability (\w+)\]\](?:{([^}]+)})?/gi;
+const savePattern =
+  /\[\[\/(r|roll|pr|publicroll|gmr|gmroll|br|broll|blindroll|sr|selfroll)Save (\w+)\]\](?:{([^}]+)})?/gi;
+const itemPattern = /\[\[\/rollItem ([^\]]+)\]\](?:{([^}]+)})?/gi;
 /**
  * Register the text enrichers to create the deferred inline roll buttons.
  */
 Hooks.once("init", () => {
   // example: [[/rollSkill ath]]
   CONFIG.TextEditor.enrichers.push({
-    pattern:
-      /\[\[\/(r|roll|pr|publicroll|gmr|gmroll|br|broll|blindroll|sr|selfroll)Skill (\w+)\]\](?:{([^}]+)})?/gi,
+    pattern: skillPattern,
     enricher: createSkill,
   });
 
   // example: [[/rollAbility str]]
   CONFIG.TextEditor.enrichers.push({
-    pattern:
-      /\[\[\/(r|roll|pr|publicroll|gmr|gmroll|br|broll|blindroll|sr|selfroll)Ability (\w+)\]\](?:{([^}]+)})?/gi,
+    pattern: abilityPattern,
     enricher: createAbility,
   });
 
   // example: [[/rollSave dex]]
   CONFIG.TextEditor.enrichers.push({
-    pattern:
-      /\[\[\/(r|roll|pr|publicroll|gmr|gmroll|br|broll|blindroll|sr|selfroll)Save (\w+)\]\](?:{([^}]+)})?/gi,
+    pattern: savePattern,
     enricher: createSave,
   });
 
   // example: [[/rollItem Dagger]]
   CONFIG.TextEditor.enrichers.push({
-    pattern: /\[\[\/rollItem ([^\]]+)\]\](?:{([^}]+)})?/gi,
+    pattern: itemPattern,
     enricher: createItem,
   });
 
   // activate listeners
   const body = $("body");
   body.on("click", "a.inline-roll-cmd", onClick);
+
+  // register setting
+  game.settings.registerMenu("inline-roll-cmd", "migrateMenu", {
+    name: "Migrate to System's Enrichers",
+    label: "Migrate",
+    icon: "fas fa-refresh",
+    type: InlineMigrationApplication,
+    restricted: true,
+  });
 });
 
 /**
@@ -105,9 +118,7 @@ function createItem(match, options) {
     if (item) img = item.img;
   }
 
-  return img
-    ? createItemButton(itemName, flavor, img)
-    : createButton("roll", "item", { itemName }, flavor, itemName);
+  return img ? createItemButton(itemName, flavor, img) : createButton("roll", "item", { itemName }, flavor, itemName);
 }
 
 /**
@@ -214,5 +225,227 @@ async function onClick(event) {
     case "item":
       dnd5e.documents.macro.rollItem(a.dataset.itemName);
       break;
+  }
+}
+
+class InlineMigration {
+  async migrateWorld() {
+    ui.notifications.info("Migrating world data to the system's enrichers", { permanent: true });
+
+    // Migrate actors
+    const actorUpdates = await this._migrateActors(game.actors);
+    Actor.updateDocuments(actorUpdates);
+
+    // Migrate items
+    const itemUpdates = this._migrateItems(game.items);
+    Item.updateDocuments(itemUpdates);
+
+    // Migrate unlinked actors on scenes
+    await this._migrateScenes(game.scenes);
+
+    ui.notifications.info("Migrating world data to the system's enrichers complete", { permanent: true });
+  }
+
+  async _migrateScenes(scenes) {
+    for (const scene of scenes) {
+      for (const token of scene.tokens) {
+        if (token.actorLink || !token.actor) continue;
+
+        const updateData = await this.migrateActorData(token.actor);
+        // if it has item updates, do them now
+        if (!foundry.utils.isEmpty(updateData.items)) {
+          await token.actor.updateEmbeddedDocuments("Item", updateData.items);
+          delete updateData.items;
+        }
+        // if there are updates to the actor, do them now
+        if (!foundry.utils.isEmpty(updateData)) await token.actor.update(updateData);
+      }
+    }
+  }
+
+  _migrateItems(items) {
+    const itemUpdates = [];
+    for (const item of items) {
+      const updateData = this.migrateItemData(item);
+      // if there are updates, batch them
+      if (!foundry.utils.isEmpty(updateData)) {
+        updateData._id = item.id;
+        itemUpdates.push(updateData);
+      }
+    }
+    return itemUpdates;
+  }
+
+  async _migrateActors(actors) {
+    const actorUpdates = [];
+    for (const actor of actors) {
+      const updateData = await this.migrateActorData(actor);
+      // if it has item updates, do them now
+      if (!foundry.utils.isEmpty(updateData.items)) {
+        debug("About to update an actor's items", actor.name, updateData.items);
+        await actor.updateEmbeddedDocuments("Item", updateData.items);
+        delete updateData.items;
+      }
+      // if there are updates to the actor, batch them
+      if (!foundry.utils.isEmpty(updateData)) {
+        updateData._id = actor.id;
+        actorUpdates.push(updateData);
+      }
+    }
+    return actorUpdates;
+  }
+
+  async migrateCompendium(pack) {
+    if (pack.locked || !["Actor", "Item", "Scene"].includes(pack.documentName)) return;
+    ui.notifications.info(`Migrating pack ${pack.title} to the system's enrichers`, { permanent: true });
+
+    for (const ids of this.idChunks(pack)) {
+      const updates = [];
+
+      switch (pack.documentName) {
+        case "Actor":
+          const actors = await pack.getDocuments({ _id__in: ids });
+          const actorUpdates = await this._migrateActors(actors);
+          Actor.updateDocuments(actorUpdates, { pack: pack.collection });
+          break;
+        case "Item":
+          const items = await pack.getDocuments({ _id__in: ids });
+          const itemUpdates = this._migrateItems(items);
+          Item.updateDocuments(itemUpdates, { pack: pack.collection });
+          break;
+        case "Scene":
+          const scenes = await pack.getDocuments({ _id__in: ids });
+          await this._migrateScenes(scenes);
+          break;
+      }
+    }
+
+    ui.notifications.info(`Migrating pack ${pack.title} to the system's enrichers complete`, { permanent: true });
+  }
+
+  async migrateActorData(actor) {
+    const updateData = {};
+
+    // Convert the different description and biography fields
+    if (actor.type === "group") {
+      const full = this.toSystemEnrichers(actor.system.description.full || "");
+      if (full) updateData["system.description.full"] = full;
+      const summary = this.toSystemEnrichers(actor.system.description.summary || "");
+      if (summary) updateData["system.description.summary"] = summary;
+    } else {
+      const value = this.toSystemEnrichers(actor.system.details.biography.value || "");
+      if (value) updateData["system.details.biography.value"] = value;
+      const pub = this.toSystemEnrichers(actor.system.details.biography.public || "");
+      if (pub) updateData["system.details.biography.public"] = pub;
+    }
+
+    // Convert items
+    const itemUpdates = actor.items.reduce((updates, item) => {
+      const itemUpdate = this.migrateItemData(item);
+      if (!foundry.utils.isEmpty(itemUpdate)) {
+        itemUpdate._id = item.id;
+        updates.push(itemUpdate);
+      }
+      return updates;
+    }, []);
+    if (!foundry.utils.isEmpty(updateData)) debug("migrateActorData item updates:", itemUpdates);
+    updateData.items = itemUpdates;
+
+    return updateData;
+  }
+
+  *idChunks(pack) {
+    const ids = pack.index.map((i) => i._id);
+    while (ids.length) {
+      yield ids.splice(0, 100);
+    }
+  }
+
+  migrateItemData(item) {
+    const updateData = {};
+
+    // Convert the description fields
+    const desc = this.toSystemEnrichers(item.system.description.value || "");
+    if (desc) updateData["system.description.value"] = desc;
+    const chat = this.toSystemEnrichers(item.system.description.chat || "");
+    if (chat) updateData["system.description.chat"] = chat;
+
+    // Not each item type has this (e.g. feature)
+    if (item.system.unidentified?.description) {
+      const unident = this.toSystemEnrichers(item.system.unidentified.description);
+      if (unident) updateData["system.unidentified.description"] = unident;
+    }
+
+    if (!foundry.utils.isEmpty(updateData)) debug("migrateItemData updates:", updateData);
+    return updateData;
+  }
+
+  toSystemEnrichers(text) {
+    let replaced = false;
+    const replacers = [
+      {
+        pattern: skillPattern,
+        replacement: (match, _, skillId, flavor) => {
+          replaced = true;
+          return flavor ? `[[/skill ${skillId}]]{${flavor}}` : `[[/skill ${skillId}]]`;
+        },
+      },
+      {
+        pattern: abilityPattern,
+        replacement: (match, _, abilityId, flavor) => {
+          replaced = true;
+          return flavor ? `[[/check ${abilityId}]]{${flavor}}` : `[[/check ${abilityId}]]`;
+        },
+      },
+      {
+        pattern: savePattern,
+        replacement: (match, _, abilityId, flavor) => {
+          replaced = true;
+          return flavor ? `[[/save ${abilityId}]]{${flavor}}` : `[[/save ${abilityId}]]`;
+        },
+      },
+      {
+        pattern: itemPattern,
+        replacement: (match, itemName, flavor) => {
+          replaced = true;
+          return flavor ? `[[/item ${itemName}]]{${flavor}}` : `[[/item ${itemName}]]`;
+        },
+      },
+    ];
+
+    const newText = replacers.reduce((t, { pattern, replacement }) => t.replaceAll(pattern, replacement), text);
+    return replaced ? newText : undefined;
+  }
+}
+// expose this globally
+globalThis.inlineMigration = new InlineMigration();
+
+class InlineMigrationApplication extends FormApplication {
+  static get defaultOptions() {
+    return foundry.utils.mergeObject(super.defaultOptions, {
+      title: "Migrate Inline Roll Commands",
+      template: "modules/inline-roll-cmd/templates/migration.hbs",
+      width: 500,
+    });
+  }
+
+  async getData(options) {
+    return {
+      packs: game.packs.contents.filter((pack) => {
+        return !pack.locked && ["Actor", "Item", "Scene"].includes(pack.documentName);
+      }),
+    };
+  }
+
+  async _updateObject(event, formData) {
+    const button = event.submitter;
+    const action = button.dataset.action;
+    if (action === "migrate-world") {
+      return inlineMigration.migrateWorld();
+    } else if (action === "migrate-pack") {
+      const packId = formData.pack;
+      const pack = game.packs.get(packId);
+      return inlineMigration.migrateCompendium(pack);
+    }
   }
 }
